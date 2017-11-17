13f8919a0264ba3806bfad836124ef77a2135429
sunny <2385585770@qq.com>
2017-11-18 01:58:31 +0800

    初始化前端项目

---
+++ task-web-front/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="less" type="text/less">
+@import "assets/css/index.less";
+</style>
